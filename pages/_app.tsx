614743c6662c95bed08c3627dfaf78cab3e552eb
negres <nataliaknegres@gmail.com>
2022-08-08 22:23:03 -0300

    fix: types

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const MyApp = () => ({ Component, pageProps }: AppProps) => {
+  return (
+    <Component {...pageProps} />
+  )
 }
 
-export default MyApp
+export default MyApp;

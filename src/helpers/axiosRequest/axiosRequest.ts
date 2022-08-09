3d2fa1d118391179b8fda4cf67a053d6a2d77b8f
negres <nataliaknegres@gmail.com>
2022-08-09 00:04:34 -0300

    fix: axios request

--- src/helpers/axiosRequest/axiosRequest.ts
+++ src/helpers/axiosRequest/axiosRequest.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { IAxiosRequest } from 'helpers/axiosRequest/axiosRequest.interfaces';
 import { stringify } from 'query-string';
 
-const axiosRequest = async ({ endpoint, data, method = 'get', headers, query, toExpress }: IAxiosRequest) => {
-  const basePath = `http://localhost:3000/${endpoint}`;
+const axiosRequest = async ({ endpoint, data, method = 'get', headers, query }: IAxiosRequest) => {
+  const basePath = `${process.env.API_HOST}/${endpoint}`;
   let param = '';
   if (query) {
     param = `?${stringify(query)}`;
